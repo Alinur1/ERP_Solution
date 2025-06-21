c9deb8cae46c0621767a60f7b389aba0775f4aa3
Md. Alinur Hossain <alinur1160@gmail.com>
2025-06-21 23:10:16 +0600

    TODO: Handle saving null values for the SoftDeleteUserAsync and UpdateUserAsync methods = completed

--- ErpBackendApi/ErpBackendApi/BLL/Services/UserService.cs
+++ ErpBackendApi/ErpBackendApi/BLL/Services/UserService.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Client;
 using static ErpBackendApi.Helper.LoggerClass;
 
-//TODO: Handle saving null values for the SoftDeleteUserAsync and UpdateUserAsync methods
+//TODO: Handle saving null values for the SoftDeleteUserAsync and UpdateUserAsync methods = completed
 //TODO: Undo deletion of the deleted users
 //TODO: Prevent update for the deleted users
 //TODO: Replace "throw new exception" with "throw new BadHttpRequestException" in AddUserAsync method
@@ -80,46 +80,30 @@ namespace ErpBackendApi.BLL.Services
         }
 
         public async Task<User> SoftDeleteUserAsync(User user)
-        {
-            try
+        {            
+            var existingUser = await _context.users.FirstOrDefaultAsync(u => u.id == user.id && u.is_deleted == false);
+            if (existingUser != null)
             {
-                var existingUser = await _context.users.FindAsync(user.id);
-                if (existingUser != null)
-                {
-                    existingUser.is_deleted = user.is_deleted;
-                    existingUser.deleted_at = DateTime.UtcNow;
-                    _context.users.Update(existingUser);
-                    await _context.SaveChangesAsync();
-                }
-                return existingUser ?? throw new KeyNotFoundException("User not found. Unable to delete user.");
+                existingUser.is_deleted = user.is_deleted;
+                existingUser.deleted_at = DateTime.UtcNow;
+                _context.users.Update(existingUser);
+                await _context.SaveChangesAsync();
             }
-            catch (Exception ex)
-            {
-                Logger("An error occurred at UserService in SoftDeleteUserAsync#1: " + ex.Message + "\n" + ex.StackTrace);
-                throw new Exception("An error occurred at UserService in SoftDeleteUserAsync#1. Please see the ERP_API_Logger.txt for more information.");
-            }
+            return existingUser ?? throw new KeyNotFoundException("User not found. Unable to delete user.");            
         }
 
         public async Task<User> UpdateUserAsync(User user)
-        {
-            try
+        {            
+            var existingUser = await _context.users.FirstOrDefaultAsync(u => u.id == user.id && u.is_deleted == false);
+            if (existingUser != null)
             {
-                var existingUser = await _context.users.FindAsync(user.id);
-                if (existingUser != null)
-                {
-                    existingUser.name = user.name;
-                    existingUser.email = user.email;
-                    existingUser.phone = user.phone;
-                    _context.users.Update(existingUser);
-                    await _context.SaveChangesAsync();
-                }
-                return existingUser ?? throw new KeyNotFoundException("User not found. Unable to update user information.");
+                existingUser.name = user.name;
+                existingUser.email = user.email;
+                existingUser.phone = user.phone;
+                _context.users.Update(existingUser);
+                await _context.SaveChangesAsync();
             }
-            catch (Exception ex)
-            {
-                Logger("An error occurred at UserService in UpdateUserAsync#1: " + ex.Message + "\n" + ex.StackTrace);
-                throw new Exception("An error occurred at UserService in UpdateUserAsync#1. Please see the ERP_API_Logger.txt for more information.");
-            }
+            return existingUser ?? throw new KeyNotFoundException("User not found. Unable to update user information.");
         }
     }
 }
