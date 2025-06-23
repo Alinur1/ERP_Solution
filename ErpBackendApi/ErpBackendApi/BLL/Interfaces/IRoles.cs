87c9535b2639da8ca2e0df411442f9bb42068911
Md. Alinur Hossain <alinur1160@gmail.com>
2025-06-23 17:33:28 +0600

    Roles operation implemented

---
+++ ErpBackendApi/ErpBackendApi/BLL/Interfaces/IRoles.cs
@@ -0,0 +1,14 @@
+﻿using ErpBackendApi.DAL.Models;
+
+namespace ErpBackendApi.BLL.Interfaces
+{
+    public interface IRoles
+    {
+        Task<IEnumerable<Role>> GetAllRolesAsync();
+        Task<Role> GetRoleByIdAsync(int id);
+        Task<Role> AddRoleAsync(Role role);
+        Task<Role> UpdateRoleAsync(Role role);
+        Task<Role> SoftDeleteRoleAsync(Role role);
+        Task<Role> UndoSoftDeleteRoleAsync(Role role);
+    }
+}
