e055bcaabc02e53652089c22f91e022acb1df05a
Md. Alinur Hossain <alinur1160@gmail.com>
2025-07-23 16:24:58 +0600

    Expense model updated

--- ErpBackendApi/ErpBackendApi/DAL/Models/Expense.cs
+++ ErpBackendApi/ErpBackendApi/DAL/Models/Expense.cs
@@ -4,10 +4,10 @@ namespace ErpBackendApi.DAL.Models
     {
         public int id { get; set; }
         public int? purchase_order_id { get; set; }
+        public int? product_id { get; set; }
         public string? description { get; set; }
         public decimal? amount { get; set; }
         public DateOnly? expense_date { get; set; }
-        public int? category_id { get; set; }
         public bool? is_deleted { get; set; }
         public DateTime? deleted_at { get; set; }
     }
