deaffedf836905cdaa6ba48b58ee39942432f871
Md. Alinur Hossain <alinur1160@gmail.com>
2025-07-29 21:26:53 +0600

    Finish implementing the AccountService

---
+++ ErpBackendApi/ErpBackendApi/BLL/Interfaces/IAccount.cs
@@ -0,0 +1,14 @@
+﻿using ErpBackendApi.DAL.Models;
+
+namespace ErpBackendApi.BLL.Interfaces
+{
+    public interface IAccount
+    {
+        Task<IEnumerable<Account>> GetAllAccountsAsync();
+        Task<Account> GetAccountByIdAsync(int id);
+        Task<Account> AddAccountAsync(Account account);
+        Task<Account> UpdateAccountAsync(Account account);
+        Task<Account> SoftDeleteAccountAsync(Account account);
+        Task<Account> UndoSoftDeleteAccountAsync(Account account);
+    }
+}
---
+++ ErpBackendApi/ErpBackendApi/BLL/Services/AccountService.cs
@@ -0,0 +1,45 @@
+﻿using ErpBackendApi.BLL.Interfaces;
+using ErpBackendApi.DAL.ERPDataContext;
+using ErpBackendApi.DAL.Models;
+
+namespace ErpBackendApi.BLL.Services
+{
+    public class AccountService : IAccount
+    {
+        private readonly AppDataContext _context;
+        public AccountService(AppDataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Account> GetAccountByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Account>> GetAllAccountsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account> AddAccountAsync(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account> UpdateAccountAsync(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account> SoftDeleteAccountAsync(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account> UndoSoftDeleteAccountAsync(Account account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
