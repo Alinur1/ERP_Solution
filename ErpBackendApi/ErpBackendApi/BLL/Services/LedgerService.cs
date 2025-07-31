8bfdb50953890bc44280a4b72fe8ae7edf2a43e7
Md. Alinur Hossain <alinur1160@gmail.com>
2025-07-31 15:59:11 +0600

    Ledger API implemented

--- ErpBackendApi/ErpBackendApi/BLL/Services/LedgerService.cs
+++ ErpBackendApi/ErpBackendApi/BLL/Services/LedgerService.cs
@@ -2,6 +2,8 @@
 using ErpBackendApi.DAL.DTOs;
 using ErpBackendApi.DAL.ERPDataContext;
 using ErpBackendApi.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using static ErpBackendApi.Utilities.Helper.LoggerClass;
 
 namespace ErpBackendApi.BLL.Services
 {
@@ -15,36 +17,121 @@ namespace ErpBackendApi.BLL.Services
 
         public async Task<IEnumerable<LedgerDTO>> GetAllLedgersAsync()
         {
-            //return await
-            //(
-            //    from l in _context.ledgers
-            //)
-            throw new NotImplementedException();
+            return await
+            (
+                from l in _context.ledgers
+                join da in _context.accounts on l.debit_account_id equals da.id into daGroup
+                from da in daGroup.DefaultIfEmpty()
+                join ca in _context.accounts on l.credit_account_id equals ca.id into caGroup
+                from ca in caGroup.DefaultIfEmpty()
+                where l.is_deleted == false
+                select new LedgerDTO
+                {
+                    id = l.id,
+                    entry_date = l.entry_date,
+                    description = l.description,
+                    debit_account_id = da != null ? da.id : null,
+                    debit_account_name = da != null && da.is_deleted == false ? da.name : null,
+                    credit_account_id = ca != null ? ca.id : null,
+                    credit_amount_name = ca != null && ca.is_deleted == false ? ca.name : null,
+                    amount = l.amount,
+                }
+            ).ToListAsync();
         }
 
-        public Task<LedgerDTO> GetLedgerByIdAsync(int id)
+        public async Task<LedgerDTO> GetLedgerByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await
+            (
+                from l in _context.ledgers
+                join da in _context.accounts on l.debit_account_id equals da.id into daGroup
+                from da in daGroup.DefaultIfEmpty()
+                join ca in _context.accounts on l.credit_account_id equals ca.id into caGroup
+                from ca in caGroup.DefaultIfEmpty()
+                where l.id == id && l.is_deleted == false
+                select new LedgerDTO
+                {
+                    id = l.id,
+                    entry_date = l.entry_date,
+                    description = l.description,
+                    debit_account_id = da != null ? da.id : null,
+                    debit_account_name = da != null && da.is_deleted == false ? da.name : null,
+                    credit_account_id = ca != null ? ca.id : null,
+                    credit_amount_name = ca != null && ca.is_deleted == false ? ca.name : null,
+                    amount = l.amount,
+                }
+            ).FirstOrDefaultAsync();
         }
 
-        public Task<Ledger> AddLedgerAsync(Ledger ledger)
+        public async Task<Ledger> AddLedgerAsync(Ledger ledger)
         {
-            throw new NotImplementedException();
+            var checkDebitAccount = await _context.accounts.FirstOrDefaultAsync(a => a.id == ledger.debit_account_id && a.is_deleted == false);
+            var checkCreditAccount = await _context.accounts.FirstOrDefaultAsync(a => a.id == ledger.credit_account_id && a.is_deleted == false);
+            if (checkDebitAccount == null)
+            {
+                Logger("Invalid account id for adding debit information.");
+                return null;
+            }
+            if (checkCreditAccount == null)
+            {
+                Logger("Invalid account id for adding credit information.");
+                return null;
+            }
+            if(checkDebitAccount == checkCreditAccount)
+            {
+                Logger("Debit and credit accounts cannot be the same.");
+                return null;
+            }
+            ledger.is_deleted = false;
+            ledger.deleted_at = null;
+            _context.ledgers.Add(ledger);
+            await _context.SaveChangesAsync();
+            return ledger;
         }
 
-        public Task<Ledger> UpdateLedgerAsync(Ledger ledger)
+        public async Task<Ledger> UpdateLedgerAsync(Ledger ledger)
         {
-            throw new NotImplementedException();
+            var existingLedger = await _context.ledgers.FirstOrDefaultAsync(l => l.id == ledger.id && l.is_deleted == false);
+            if (existingLedger == null)
+            {
+                Logger("Unable to update ledger information. Invalid ledger ID.");
+                return null;
+            }
+            existingLedger.entry_date = ledger.entry_date;
+            existingLedger.description = ledger.description;
+            existingLedger.debit_account_id = ledger.debit_account_id;
+            existingLedger.credit_account_id = ledger.credit_account_id;
+            existingLedger.amount = ledger.amount;
+            await _context.SaveChangesAsync();
+            return existingLedger;
         }
 
-        public Task<Ledger> SoftDeleteLedgerAsync(Ledger ledger)
+        public async Task<Ledger> SoftDeleteLedgerAsync(Ledger ledger)
         {
-            throw new NotImplementedException();
+            var existingLedger = await _context.ledgers.FirstOrDefaultAsync(l => l.id == ledger.id && l.is_deleted == false);
+            if (existingLedger == null)
+            {
+                Logger("Unable to delete ledger information. Invalid ledger ID.");
+                return null;
+            }
+            existingLedger.is_deleted = true;
+            existingLedger.deleted_at = DateTime.UtcNow;
+            await _context.SaveChangesAsync();
+            return existingLedger;
         }
 
-        public Task<Ledger> UndoSoftDeleteLedgerAsync(Ledger ledger)
+        public async Task<Ledger> UndoSoftDeleteLedgerAsync(Ledger ledger)
         {
-            throw new NotImplementedException();
+            var existingLedger = await _context.ledgers.FirstOrDefaultAsync(l => l.id == ledger.id && l.is_deleted == true);
+            if (existingLedger == null)
+            {
+                Logger("Unable to restore deleted ledger information. Invalid ledger ID.");
+                return null;
+            }
+            existingLedger.is_deleted = false;
+            existingLedger.deleted_at = null;
+            await _context.SaveChangesAsync();
+            return existingLedger;
         }
     }
 }
---
+++ ErpBackendApi/ErpBackendApi/Controllers/LedgerController.cs
@@ -0,0 +1,80 @@
+﻿using ErpBackendApi.BLL.Interfaces;
+using ErpBackendApi.DAL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ErpBackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LedgerController : ControllerBase
+    {
+        private readonly ILedger _iLedger;
+        public LedgerController(ILedger iLedger)
+        {
+            _iLedger = iLedger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllLedgers()
+        {
+            var operation_GetAllLedgers = await _iLedger.GetAllLedgersAsync();
+            return Ok(operation_GetAllLedgers);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetLedgerById(int id)
+        {
+            var operation_GetLedgerById = await _iLedger.GetLedgerByIdAsync(id);
+            if (operation_GetLedgerById == null)
+            {
+                return NotFound("Ledger information not found. Invalid ledger ID.");
+            }
+            return Ok(operation_GetLedgerById);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddLedger(Ledger ledger)
+        {
+            var operation_AddLedger = await _iLedger.AddLedgerAsync(ledger);
+            if (operation_AddLedger == null)
+            {
+                return NotFound("Unable to add ledger information. Something went wrong.");
+            }
+            return Ok("Ledger information added successfully.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateLedger(Ledger ledger)
+        {
+            var operation_UpdateLedger = await _iLedger.UpdateLedgerAsync(ledger);
+            if (operation_UpdateLedger == null)
+            {
+                return NotFound("Unable to update ledger information. Invalid ledger ID.");
+            }
+            return Ok("Ledger information updated successfully.");
+        }
+
+        [HttpPut("delete")]
+        public async Task<IActionResult> SoftDeleteLedger(Ledger ledger)
+        {
+            var operation_SoftDeleteLedger = await _iLedger.SoftDeleteLedgerAsync(ledger);
+            if (operation_SoftDeleteLedger == null)
+            {
+                return NotFound("Unable to delete ledger information. Invalid ledger ID.");
+            }
+            return Ok("Ledger information deleted successfully.");
+        }
+
+        [HttpPut("undo-delete")]
+        public async Task<IActionResult> UndoSoftDeleteLedger(Ledger ledger)
+        {
+            var operation_UndoSoftDeleteLedger = await _iLedger.UndoSoftDeleteLedgerAsync(ledger);
+            if (operation_UndoSoftDeleteLedger == null)
+            {
+                return NotFound("Unable to restore deleted ledger information. Invalid ledger ID.");
+            }
+            return Ok("Ledger information restored successfully.");
+        }
+    }
+}
