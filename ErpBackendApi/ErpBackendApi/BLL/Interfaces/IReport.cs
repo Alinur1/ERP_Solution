418f1250fe17c07467d3260077cc1a32755d99f8
Md. Alinur Hossain <alinur1160@gmail.com>
2025-07-31 16:21:02 +0600

    DTO and Interface for report implemented

---
+++ ErpBackendApi/ErpBackendApi/BLL/Interfaces/IReport.cs
@@ -0,0 +1,13 @@
+﻿using ErpBackendApi.DAL.DTOs;
+using ErpBackendApi.DAL.Models;
+
+namespace ErpBackendApi.BLL.Interfaces
+{
+    public interface IReport
+    {
+        Task<IEnumerable<ReportDTO>> GetAllReportsAsync();
+        Task<ReportDTO> GetReportByIdAsync(int id);
+        Task<Report> AddReportAsync(Report report);
+        Task<Report> UpdateReportAsync(Report report);
+    }
+}
---
+++ ErpBackendApi/ErpBackendApi/DAL/DTOs/ReportDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace ErpBackendApi.DAL.DTOs
+{
+    public class ReportDTO
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public string? module { get; set; }
+        public int? created_by { get; set; }
+        public string? created_by_name { get; set; }
+        public DateTime? created_at { get; set; }
+        public string? filters_json { get; set; }
+    }
+}
