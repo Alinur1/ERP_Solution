c1ac6ac96f69475cb813f6c385c6f56fe9ddc5a4
Md. Alinur Hossain <alinur1160@gmail.com>
2025-08-02 16:38:37 +0600

    CompanyProfile Service implemented

---
+++ ErpBackendApi/ErpBackendApi/BLL/Interfaces/ICompanyProfile.cs
@@ -0,0 +1,13 @@
+﻿using ErpBackendApi.DAL.Models;
+
+namespace ErpBackendApi.BLL.Interfaces
+{
+    public interface ICompanyProfile
+    {
+        Task<IEnumerable<CompanyProfile>> GetCompanyProfileAsync();
+        Task<CompanyProfile> AddCompanyProfileAsync(CompanyProfile companyProfile);
+        Task<CompanyProfile> UpdateCompanyProfileAsync(CompanyProfile companyProfile);
+        Task<CompanyProfile> SoftDeleteCompanyProfileAsync(CompanyProfile companyProfile);
+        Task<CompanyProfile> UndoSoftDeleteCompanyProfileAsync(CompanyProfile companyProfile);
+    }
+}
---
+++ ErpBackendApi/ErpBackendApi/BLL/Services/CompanyProfileService.cs
@@ -0,0 +1,81 @@
+﻿using ErpBackendApi.BLL.Interfaces;
+using ErpBackendApi.DAL.ERPDataContext;
+using ErpBackendApi.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using static ErpBackendApi.Utilities.Helper.LoggerClass;
+
+//TODO: Make the company profile service compatible for one company only.
+
+namespace ErpBackendApi.BLL.Services
+{
+    public class CompanyProfileService : ICompanyProfile
+    {
+        private readonly AppDataContext _context;
+        public CompanyProfileService(AppDataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<CompanyProfile>> GetCompanyProfileAsync()
+        {
+            return await _context.company_profile.ToListAsync();
+        }
+
+        public async Task<CompanyProfile> AddCompanyProfileAsync(CompanyProfile companyProfile)
+        {
+            companyProfile.is_deleted = false;
+            companyProfile.deleted_at = null;
+            _context.company_profile.Add(companyProfile);
+            await _context.SaveChangesAsync();
+            return companyProfile;
+        }
+
+        public async Task<CompanyProfile> UpdateCompanyProfileAsync(CompanyProfile companyProfile)
+        {
+            var existingCompany = await _context.company_profile.FirstOrDefaultAsync(c => c.id == companyProfile.id && c.is_deleted == false);
+            if (existingCompany == null)
+            {
+                Logger("Company not found. Company profile can't be updated.");
+                return null;
+            }
+            existingCompany.company_name = companyProfile.company_name;
+            existingCompany.address = companyProfile.address;
+            existingCompany.email = companyProfile.email;
+            existingCompany.phone = companyProfile.phone;
+            existingCompany.tax_number = companyProfile.tax_number;
+            existingCompany.logo = companyProfile.logo;
+            await _context.SaveChangesAsync();
+            return existingCompany;
+        }
+
+        public async Task<CompanyProfile> SoftDeleteCompanyProfileAsync(CompanyProfile companyProfile)
+        {
+            var existingCompany = await _context.company_profile.FirstOrDefaultAsync(c => c.id == companyProfile.id && c.is_deleted == false);
+            if (existingCompany == null)
+            {
+                Logger("Company not found. Company profile can't be deleted.");
+                return null;
+            }
+            existingCompany.is_deleted = true;
+            existingCompany.deleted_at = DateTime.UtcNow;
+            _context.company_profile.Update(existingCompany);
+            await _context.SaveChangesAsync();
+            return existingCompany;
+        }
+
+        public async Task<CompanyProfile> UndoSoftDeleteCompanyProfileAsync(CompanyProfile companyProfile)
+        {
+            var existingCompany = await _context.company_profile.FirstOrDefaultAsync(c => c.id == companyProfile.id && c.is_deleted == true);
+            if (existingCompany == null)
+            {
+                Logger("Company not found. Company profile can't be restored.");
+                return null;
+            }
+            existingCompany.is_deleted = false;
+            existingCompany.deleted_at = null;
+            _context.company_profile.Update(existingCompany);
+            await _context.SaveChangesAsync();
+            return existingCompany;
+        }
+    }
+}
