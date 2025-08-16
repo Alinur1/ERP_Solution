96fe677008f666ac11ee02cfec1c6604b3fefcb3
Md. Alinur Hossain <alinur1160@gmail.com>
2025-08-16 16:39:20 +0600

    A static class to hash the password is added

---
+++ ErpBackendApi/ErpBackendApi/Utilities/Helper/PasswordHasher.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ErpBackendApi.Utilities.Helper
+{
+    public static class PasswordHasher
+    {
+        public static string HashPass(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentException("Password cannot be null or empty.", nameof(password));
+
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(password);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
+                // Convert hash bytes to hex string
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hashBytes)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
