dde817fcb8a2f85a8e7c587f9cd7428d385f0d32
Md. Alinur Hossain <alinur1160@gmail.com>
2025-08-17 15:46:54 +0600

    PaginatedResult added
    Refer to the TODO in this class

---
+++ ErpBackendApi/ErpBackendApi/Utilities/Helper/PaginatedResult.cs
@@ -0,0 +1,13 @@
+﻿namespace ErpBackendApi.Utilities.Helper
+{
+    public class PaginatedResult<T>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+        public IEnumerable<T> Data { get; set; }
+    }
+}
+
+//TODO: Add pagination as necessity
