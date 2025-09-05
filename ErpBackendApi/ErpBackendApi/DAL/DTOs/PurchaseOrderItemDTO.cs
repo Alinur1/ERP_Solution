4cb2d012ad5110d25455c21bf9cfe1810f5761c8
Md. Alinur Hossain <alinur1160@gmail.com>
2025-09-05 17:16:41 +0600

    amount column added instead of unit_price in PurchaseOrderItems

--- ErpBackendApi/ErpBackendApi/DAL/DTOs/PurchaseOrderItemDTO.cs
+++ ErpBackendApi/ErpBackendApi/DAL/DTOs/PurchaseOrderItemDTO.cs
@@ -7,7 +7,7 @@
         public int? product_id { get; set; }
         public string? product_name { get; set; }
         public int? quantity { get; set; }
-        public decimal? unit_price { get; set; }
+        public decimal? amount { get; set; }
         public decimal? discount { get; set; }
         public bool? is_deleted { get; set; }
         public DateTime? deleted_at { get; set; }
